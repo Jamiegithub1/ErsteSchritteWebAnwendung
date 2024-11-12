ca3933eb844db2e980c2b692ed3a7c8ed656151a
Jamiegithub1 <jamie.weber@stud.fra-uas.de>
2024-11-12 15:35:06 +0100

    Ich habe meine ersten Ausgabe für Web gemacht

---
+++ hello/src/main/java/edu/fra/uas/hello/controller/HelloWorldController.java
@@ -0,0 +1,27 @@
+package edu.fra.uas.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+
+
+
+
+@Controller
+public class HelloWorldController {
+
+
+@RequestMapping(value = "hello", method= RequestMethod.GET)
+@ResponseBody
+public String getHelloWorld(){
+    return "<h1>Hello World Spring Boo! </h1>";
+}
+
+
+
+}
